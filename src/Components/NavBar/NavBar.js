4f655a21fb86ec54ef6a0a8d107b2b7158d295ae
MD.Mahim Khan <mdmahimkhan77250@gmail.com>
2023-07-26 00:31:28 +0600

    Ad active status

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './navbar.css';
 
 const NavBar = () => {
+
   return (
     <div className="flex justify-between px-9 p-4 bg-orange-400 sticky top-0">
         <div className="gfont text-2xl">IQ knowledge</div>
-      <div>
-        <Link className="mr-4" to="/">Topics</Link>
-        <Link className="mr-4" to="/static">Statistic</Link>
-        <Link className="mr-4" to="/blog">Blog</Link>
+      <div className="link">
+        <NavLink className={({isActive}) => isActive ? 'color' : undefined} to="/">Topics</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'color' : undefined} to="/static">Statistic</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'color' : undefined} to="/blog">Blog</NavLink>
       </div>
     </div>
   );
 };
-
 export default NavBar;
